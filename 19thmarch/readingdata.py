b447a976aa921f19f12317dbf8d6a6052c355339
Gopika Ajit <gopikaajit@gmail.com>
2025-03-19 12:23:16 +0530

    first commit

---
+++ 19thmarch/readingdata.py
@@ -0,0 +1,5 @@
+#program to read the data from user
+#I read name and age and print it
+Name = input("Enter your name = ")
+Age = input("Enter your age = ")
+print("Your name is ", Name, "and you are ", Age,"years old")
