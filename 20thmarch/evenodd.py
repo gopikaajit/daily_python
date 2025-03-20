6082350a4b01a223faf4586bd1e678b58a233dd0
Gopika Ajit <gopikaajit@gmail.com>
2025-03-20 10:43:51 +0530

    adding evenodd

---
+++ 20thmarch/evenodd.py
@@ -0,0 +1,7 @@
+#program to check even or odd
+input_user = int(input("Enter a number="))
+if(input_user%2==0):
+    print("You've entered an even number.")
+
+else:
+    print("You've entered an odd number.")
